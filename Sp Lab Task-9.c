e0973bbd432a6fa93f55e13d3a6db95f2b2cb2ab
Rizon794 <mahbubhossainrizon794@gmail.com>
2024-10-04 22:37:10 +0600

    Sp Lab Task-9

---
+++ Sp Lab Task-9.c
@@ -0,0 +1,13 @@
+//Write a C program to display the multiplication table of a given number using a for loop
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("Table of=");
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        printf("%d*%d=%d\n",n,i,n*i);
+    }
+    return 0;
+}
