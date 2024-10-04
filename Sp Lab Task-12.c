63ce182306fc31f48dee263a52f9ba9d9fd0ac27
Rizon794 <mahbubhossainrizon794@gmail.com>
2024-10-04 22:43:25 +0600

    Sp Lab Task-12

---
+++ Sp Lab Task-12.c
@@ -0,0 +1,20 @@
+//Write a program that takes an integer as input and calculates the sum of its digits using a while loop
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter any digit:");
+    scanf("%d",&n);
+    int sum=0;
+    int temp=n;
+    while(temp!=0)
+        {
+            int rem=temp%10;
+            sum=sum+rem;
+            temp=temp/10;
+        }
+        printf("The sum of the digit:%d",sum);
+        return 0;
+        }
+
+
