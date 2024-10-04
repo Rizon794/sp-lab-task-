11092d69b721451d3762d1a32a40309d4fd24dda
Rizon794 <mahbubhossainrizon794@gmail.com>
2024-10-04 22:41:36 +0600

    Sp Lab Task-11

---
+++ Sp Lab Task-11.c
@@ -0,0 +1,28 @@
+//Write a C program to generate the Fibonacci series up to n terms using a for loop
+#include<stdio.h>
+int main()
+{
+    int n, first_num=0,second_num=1,Fibo=0;
+    printf("Enter the range of series:");
+    scanf("%d",&n);
+
+    for(int i=0;i<=n;i++)
+    {
+        if(i<=1)
+    {
+        Fibo=i;
+
+    }
+    else
+        {
+
+            Fibo=first_num+second_num;
+            first_num=second_num;
+            second_num=Fibo;
+
+        }
+        printf(" %d",Fibo);
+        }
+        return 0;
+
+        }
